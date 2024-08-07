a7d71b41787ebe1b7511d39b5015ca8cc4ea13c7
Anil Putchakayala <anil252anil@gmail.com>
2024-08-07 13:26:16 +0530

    Initial Commit

---
+++ target/ProjectCountries-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO countries (name, capital) VALUES ('United States', 'Washington, D.C.');
+INSERT INTO countries (name, capital) VALUES ('Canada', 'Ottawa');
+INSERT INTO countries (name, capital) VALUES ('United Kingdom', 'London');
+INSERT INTO countries (name, capital) VALUES ('Germany', 'Berlin');
+INSERT INTO countries (name, capital) VALUES ('France', 'Paris');
